1685a3d5179cf9aecc8c8cb3f3d86e7e127ff913
asselzhl <asselzhly@gmail.com>
2024-03-25 11:52:39 +0500

    implement HourlyWeather section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
+import pin from "./assets/svg/pin.svg";
 
-import pin from './assets/svg/pin.svg';
-
+import HourlyWeather from "./components/HourlyWeather";
 
 const style = {
   container: `bg-[#D69E36] flex flex-col h-screen px-6 py-12 text-white`,
@@ -14,77 +14,98 @@ const style = {
   weatherTodayTitle: `text-2xl font-medium mb-3 lg:text-5xl text-center lg:text-left`,
   weatherTodayIcon: `mb-3`,
   weatherTodayTemp: `text-6xl font-medium mb-4`,
-  weatherTodayDate: `text-lg` 
-}
-
-const hourly = 'https://api.openweathermap.org/data/2.5/forecast?q=london&appid=b94e2326dd926a1013c56922ede2651e'
-
+  weatherTodayDate: `text-lg`,
+};
 
 function App() {
   const [data, setData] = useState({});
-  const [city, setCity] = useState('London');
-  const [day, setDay] = useState('');
+  const [city, setCity] = useState("London");
+  const [day, setDay] = useState("");
 
-  const apiKey = 'b94e2326dd926a1013c56922ede2651e';
+  const apiKey = "b94e2326dd926a1013c56922ede2651e";
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city ? city : 'london'}&appid=${apiKey}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${
+    city ? city : "london"
+  }&appid=${apiKey}&units=metric`;
 
   const getWeatherData = () => {
     axios.get(url).then((response) => {
       setData(response.data);
-    })
+    });
   };
 
   const getDate = () => {
-    const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const dayNames = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
     const day = new Date().getDay();
 
     const date = new Date().toDateString().slice(4);
-    
+
     setDay(`${dayNames[day]} | ${date}`);
-  }
+  };
 
   const searchCity = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       getWeatherData();
     }
-  }
+  };
 
   useEffect(() => {
     const handleLoad = () => {
       getWeatherData();
       getDate();
     };
-    window.addEventListener('load', handleLoad);
+    window.addEventListener("load", handleLoad);
     return () => {
-      window.removeEventListener('load', handleLoad);
+      window.removeEventListener("load", handleLoad);
     };
   }, []);
-
-  
 
   return (
     <div className={style.container}>
       <div className={style.inputContainer}>
-        <div><img src={pin} alt="" /></div>
-        <input type="text" className={style.city} value={city} onChange={event => setCity(event.target.value)} onKeyPress={searchCity} />
+        <div>
+          <img src={pin} alt="" />
+        </div>
+        <input
+          type="text"
+          className={style.city}
+          value={city}
+          onChange={(event) => setCity(event.target.value)}
+          onKeyPress={searchCity}
+        />
       </div>
 
       <div className={style.weatherToday}>
-        <div className={style.weatherTodayTitle}>{data.weather ? data.weather[0].main : null}</div>
-        <div className='lg:row-start-1 lg:row-end-3 lg:col-start-2 lg:text-right lg:place-self-end'>
-          <i className={`${style.weatherTodayIcon} owf owf-13x owf-${data.weather ? data.weather[0].id : ''}`}></i>
+        <div className={style.weatherTodayTitle}>
+          {data.weather ? data.weather[0].main : null}
         </div>
-        <div className='text-center lg:text-left'>
-          <div className={style.weatherTodayTemp}>{data.main ? Math.round(data.main.temp) : null}°C</div>
+        <div className="lg:row-start-1 lg:row-end-3 lg:col-start-2 lg:text-right lg:place-self-end">
+          <i
+            className={`${style.weatherTodayIcon} owf owf-13x owf-${
+              data.weather ? data.weather[0].id : ""
+            }`}
+          ></i>
+        </div>
+        <div className="text-center lg:text-left">
+          <div className={style.weatherTodayTemp}>
+            {data.main ? data.main.temp.toFixed() : null}°C
+          </div>
           <h5 className={style.weatherTodayDate}>{day}</h5>
         </div>
       </div>
       <div>
-      
+        <HourlyWeather city={city} />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/HourlyWeather.jsx
@@ -0,0 +1,136 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Line } from "react-chartjs-2";
+
+import clock from "../assets/svg/clock.svg";
+
+import {
+  Chart as ChartJS,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+} from "chart.js";
+
+import ChartDataLabels from "chartjs-plugin-datalabels";
+
+ChartJS.register(
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  ChartDataLabels
+);
+
+const HourlyWeather = ({ city }) => {
+  const [data, setData] = useState({});
+  const [labels, setLabels] = useState([]);
+  const [degrees, setDegrees] = useState([]);
+  const [windSpeed, setWindSpeed] = useState([]);
+
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=4&appid=b94e2326dd926a1013c56922ede2651e&units=metric`;
+  const getWeatherData = () => {
+    let labelsArray = [];
+    let degreesArray = [];
+    let windSpeedArray = [];
+    axios.get(url).then((response) => {
+      setData(response.data);
+      response.data.list.forEach((item) => {
+        labelsArray.push(`${new Date(item.dt_txt).getHours()}:00`);
+        degreesArray.push(item.main.temp.toFixed());
+        windSpeedArray.push(`${item.wind.speed}km/h`);
+      });
+      
+      setLabels(labelsArray);
+      setDegrees(degreesArray);
+      setWindSpeed(windSpeedArray);
+    });
+  };
+
+  useEffect(() => {
+    const handleLoad = () => {
+      getWeatherData();
+    };
+    window.addEventListener("load", handleLoad);
+    return () => {
+      window.removeEventListener("load", handleLoad);
+    };
+  }, []);
+
+  const calculate = (i) => {
+    return windSpeed[i];
+  };
+  const lineData = {
+    labels: labels,
+    datasets: [
+      {
+        data: degrees,
+        borderColor: "#FFC355",
+        pointRadius: 0,
+        tension: 0.4,
+      },
+    ],
+  };
+  const options = {
+    layout: {
+        padding: 40
+    },
+    plugins: {
+      legend: true,
+      datalabels: {
+        labels: {
+          title: {
+            align: "end",
+            color: "white",
+            formatter: function (value, context) {
+              return value + "°";
+            },
+          },
+          value: {
+            align: "bottom",
+            color: "white",
+            textAlign: "center",
+            formatter: function (context, chart_obj) {
+              return (
+                calculate(chart_obj.dataIndex) +
+                "\n" +
+                chart_obj.chart.config._config.data.labels[chart_obj.dataIndex]
+              );
+            },
+          },
+        },
+      },
+    },
+    scales: {
+      x: {
+        
+        // min: 0,
+        // max: 6,
+        grid: {
+          display: false,
+        },
+        display: false,
+      },
+      y: {
+        min: Math.min(...degrees) - 5,
+        max: Math.max(...degrees) + 5,
+        grid: {
+          display: false,
+        },
+        display: false,
+      },
+    },
+  };
+
+  return (
+    <div className="bg-[#DFAE53]/80 rounded-lg lg:rounded-[40px]">
+      <div className="flex gap-x-1.5 p-3 lg:px-[35px]">
+        <img src={clock} alt="" />
+        24-hour forecast
+      </div>
+      <Line data={lineData} options={options} />
+    </div>
+  );
+};
+
+export default HourlyWeather;
